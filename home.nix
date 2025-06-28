d783eb25f64ac67d0c7d5395c20c470872bfb1b2
Harrizon <harryalex0281@hotmail.com>
2025-06-27 23:33:02 -0500

    Changed neovim dotfiles from github to local

--- home.nix
+++ home.nix
@@ -73,10 +73,16 @@ in
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".config/nvim" = {
-      source = inputs.nvim-config;
-      recursive = true;
-    };
+    # ".config/nvim" = {
+    #   source = inputs.nvim-config;
+    #   recursive = true;
+    # };
+
+    # ".config/nvim/ftplugin".source = ./dotfiles/nvim/ftplugin;
+    # ".config/nvim/init.lua".source = ./dotfiles/nvim/init.lua;
+    # ".config/nvim/lua".source = ./dotfiles/nvim/lua;
+
+    ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/home-manager/dotfiles/nvim";
 
     ".config/hyprshade".source = ./dotfiles/hyprshade;
 
