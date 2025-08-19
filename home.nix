7162283a47c2bc69d051cc1a7098ccad87e91809
Harrizon <harryalex0281@hotmail.com>
2025-08-19 08:43:09 -0500

    removed hyprshade reference

--- home.nix
+++ home.nix
@@ -141,9 +141,6 @@ in
 
   wayland.windowManager.hyprland = {
     enable = true;
-    systemd.extraCommands = [
-      "systemctl --user enable --now hyprshade.timer"
-    ];
     settings = {
       exec-once = [
         "${pkgs.hyprpaper}/bin/hyprpaper"
