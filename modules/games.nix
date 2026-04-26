0351c93789517e3b2caf93c90bee5f7f7dd1609e
Harrizon <harryalex0281@hotmail.com>
2026-04-26 14:26:50 -0500

    added nvidia support for gaming

--- modules/games.nix
+++ modules/games.nix
@@ -1,29 +1,39 @@
 { pkgs, pkgs-unstable, ... }:
+with pkgs; let
+  patchDesktop = pkg: appName: from: to: lib.hiPrio (
+    pkgs.runCommand "$patched-desktop-entry-for-${appName}" {} ''
+      ${coreutils}/bin/mkdir -p $out/share/applications
+      ${gnused}/bin/sed 's#${from}#${to}#g' < ${pkg}/share/applications/${appName}.desktop > $out/share/applications/${appName}.desktop
+      '');
+  GPUOffloadApp = pkg: desktopName: patchDesktop pkg desktopName "^Exec=" "Exec=nvidia-offload ";
+in
 {
   home.packages = [
-    pkgs.openspades
-    pkgs.doomrunner
-    pkgs-unstable.uzdoom
-    pkgs-unstable.lutris-free
-    pkgs.protonup-rs
+    pkgs.protonup-rs 
 
-    pkgs.duckstation
-    (pkgs.retroarch.withCores (cores: with cores; [
+    (GPUOffloadApp pkgs.openspades "openspades")
+    (GPUOffloadApp pkgs.doomrunner "DoomRunner")
+    (GPUOffloadApp pkgs-unstable.uzdoom "org.zdoom.UZDoom")
+    (GPUOffloadApp pkgs-unstable.lutris-free "net.lutris.Lutris")
+
+    (GPUOffloadApp pkgs.duckstation "org.duckstation.DuckStation")
+    (GPUOffloadApp (pkgs.retroarch.withCores (cores: with cores; [
       mame
       fbneo
       snes9x
       swanstation
       mesen
       genesis-plus-gx
-    ]))
+    ])) "com.libretro.RetroArch")
+
     # pkgs.clonehero
-    (pkgs.clonehero.overrideAttrs (previousAttrs: {
+    (GPUOffloadApp (pkgs.clonehero.overrideAttrs (previousAttrs: {
       version = "1.1.0.4261-PTB";
       src = pkgs.fetchurl {
         url = "https://github.com/clonehero-game/releases/releases/download/v1.1.0.4261-PTB/clonehero-linux.tar.xz";
         hash = "sha256-Yfbd8TqTZ0IYxMIY5TmsxTfD/Bz/anV0dgP1v13ders=";
       };
-    }))
+    })) "clonehero")
   ];
 
   home.file = {
