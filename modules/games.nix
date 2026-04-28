913e2825a8738180e7bbfca86854975b72ac0f53
Harrizon <harryalex0281@hotmail.com>
2026-04-28 10:09:55 -0500

    added games

--- modules/games.nix
+++ modules/games.nix
@@ -1,43 +1,49 @@
 { pkgs, pkgs-unstable, ... }:
-with pkgs; let
-  patchDesktop = pkg: appName: from: to: lib.hiPrio (
-    pkgs.runCommand "$patched-desktop-entry-for-${appName}" {} ''
-      ${coreutils}/bin/mkdir -p $out/share/applications
-      ${gnused}/bin/sed 's#${from}#${to}#g' < ${pkg}/share/applications/${appName}.desktop > $out/share/applications/${appName}.desktop
-      '');
-  GPUOffloadApp = pkg: desktopName: patchDesktop pkg desktopName "^Exec=" "Exec=nvidia-offload ";
-in
 {
   home.packages = [
     pkgs.protonup-rs 
 
-    (GPUOffloadApp pkgs.openspades "openspades")
-    (GPUOffloadApp pkgs.doomrunner "DoomRunner")
-    (GPUOffloadApp pkgs-unstable.uzdoom "org.zdoom.UZDoom")
-    (GPUOffloadApp pkgs-unstable.lutris-free "net.lutris.Lutris")
+    pkgs.openspades
+    pkgs.doomrunner
+    pkgs-unstable.uzdoom
 
-    (GPUOffloadApp pkgs.duckstation "org.duckstation.DuckStation")
-    (GPUOffloadApp (pkgs.retroarch.withCores (cores: with cores; [
+    pkgs-unstable.lutris-free
+    pkgs-unstable.faugus-launcher
+
+    pkgs.duckstation
+    (pkgs.retroarch.withCores (cores: with cores; [
       mame
       fbneo
       snes9x
       swanstation
       mesen
       genesis-plus-gx
-    ])) "com.libretro.RetroArch")
+    ]))
 
     # pkgs.clonehero
-    (GPUOffloadApp (pkgs.clonehero.overrideAttrs (previousAttrs: {
+    (pkgs.clonehero.overrideAttrs (previousAttrs: {
       version = "1.1.0.4261-PTB";
       src = pkgs.fetchurl {
         url = "https://github.com/clonehero-game/releases/releases/download/v1.1.0.4261-PTB/clonehero-linux.tar.xz";
         hash = "sha256-Yfbd8TqTZ0IYxMIY5TmsxTfD/Bz/anV0dgP1v13ders=";
       };
-    })) "clonehero")
+    }))
+
+    pkgs-unstable.protontricks
   ];
 
   home.file = {
     ".local/share/openspades".source = ./dotfiles/openspades;
     ".config/gzdoom/gzdoom.ini".source = ./dotfiles/gzdoom/gzdoom.ini;
+  };
+
+  programs.mangohud = {
+    enable = true;
+    enableSessionWide = false;
+    settings = {
+      full = true;
+      no_display = true;
+      cpu_load_change = true;
+    };
   };
 }
