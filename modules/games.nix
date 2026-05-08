b615d3a09dcc54fa9055825e306d52e5c4436730
Harrizon <harryalex0281@hotmail.com>
2026-05-07 23:36:05 -0500

    improve proton compatibility with faugus launcher and add xwvrn for meta quest

--- modules/games.nix
+++ modules/games.nix
@@ -1,4 +1,4 @@
-{ pkgs, pkgs-unstable, ... }:
+{ config, pkgs, pkgs-unstable, ... }:
 {
   home.packages = [
     pkgs.protonup-rs 
@@ -7,8 +7,11 @@
     pkgs.doomrunner
     pkgs-unstable.uzdoom
 
-    pkgs-unstable.lutris-free
+    pkgs-unstable.wivrn
+
     pkgs-unstable.faugus-launcher
+    pkgs.goverlay
+    pkgs.vulkan-tools
 
     pkgs.duckstation
     (pkgs.retroarch.withCores (cores: with cores; [
@@ -30,10 +33,12 @@
     }))
 
     pkgs-unstable.protontricks
+    pkgs.gamescope
+    pkgs.gamescope-wsi
   ];
 
   home.file = {
-    ".local/share/openspades".source = ./dotfiles/openspades;
+    ".local/share/openspades".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/openspades;
     ".config/gzdoom/gzdoom.ini".source = ./dotfiles/gzdoom/gzdoom.ini;
   };
 
@@ -41,9 +46,32 @@
     enable = true;
     enableSessionWide = false;
     settings = {
-      full = true;
-      no_display = true;
-      cpu_load_change = true;
+      legacy_layout = false;
+
+      background_alpha = 0.6;
+      round_corners = 0;
+      background_color = "000000";
+
+      font_size = 24;
+      text_color = "FFFFFF";
+      position = "top-left";
+
+      pci_dev = "0:01:00.0";
+      table_columns = 3;
+      gpu_text = "GPU";
+      gpu_stats = true;
+      gpu_temp = true;
+      gpu_color = "2E9762";
+      cpu_text = "CPU";
+
+      fps = true;
+      fps_limit_method = "late";
+      toggle_fps_limit = "Shift_L+F1";
+
+      fps_limit = 60;
+      fps_color_change = true;
+      fps_color = "B22222,FDFD09,39F900";
+      fps_value = "30,60";
     };
   };
 }
