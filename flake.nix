badd7c1c64599f437dfdc6195a71d3d02d9efdc7
Harrizon <harryalex0281@hotmail.com>
2026-05-23 23:00:33 -0500

    removed trash openspades files/logs

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+      pkgs-unstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true;};
     in {
       homeConfigurations."harry" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
